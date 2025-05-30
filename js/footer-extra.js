1d499c76f0391217b21bf6aef53f39574e1a7a0a
Lucy <2272013306@qq.com>
2025-05-30 23:32:25 +0800

    Site updated: 2025-05-30 23:32:24

---
+++ js/footer-extra.js
@@ -0,0 +1,16 @@
+// 网站运行时长
+function show_runtime() {
+  const start = new Date("2025/05/29 22:00:00");
+  const now = new Date();
+  const diff = now - start;
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+  const runtime = `本站已运行 ${days} 天 ${hours} 小时 ${minutes} 分 ${seconds} 秒`;
+  console.log(runtime);
+  const el = document.getElementById('runtime_span');
+  if (el) el.innerText = runtime;
+}
+show_runtime();
+setInterval(show_runtime, 1000);
